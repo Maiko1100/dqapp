ee35c4fc2ade341275b01b17d58aefbfb3e3b1de
Maiko1100 <maiko1100@gmail.com>
2016-11-10 17:57:06 +0100

    test

--- resources/views/emails/brief/meten.blade.php
+++ resources/views/emails/brief/meten.blade.php
@@ -1,10 +1,22 @@
-<link rel="stylesheet" href="{{asset("assets/css/email/email.css")}}">
+<style>
+    @font-face {
+        font-family: square721;
+        src: url("../font/FontHeader.tff");
+    }
+</style>
 
+<div style="width: 100%; margin-top: 30px;">
+    <div style="width: 80%; margin: 0 auto;">
+        <div style="display: inline-block; float: left; background-image: url(http://dqglas.nl/wp-content/uploads/2016/11/dqlogo.png); background-size: 290px; background-repeat: no-repeat; height: 150px; width: 290px;"></div>
+        <div style="display: inline-block; float: right; color: #DF4D52; font-size: 20px; line-height: 40px; font-family: square721;">
+            <b>Het glasservice bedrijf in de regio Amsterdam &amp; Diemen</b> <br> 24 uur per dag bereikbaar via 020 66 50 205</div>
+    </div>
+</div>
 <div class="test">Geachte bewoners(s),<br></div>
 <br>
 Via deze email willen wij u laten weten dat wij het glas en/of roosters komen opmeten op:<br>
 <br>
-{{--<b>{{ $datum }} {{ $tijd }}</b><br>--}}
+<b>{{ $datum }} {{ $tijd }}</b><br>
 <br>
 Mocht het zo zijn dat u niet thuis kunt zijn en/of blijven, dan vragen wij u<br>
 vriendelijk ons daarvan op de hoogte te brengen.<br>
@@ -20,5 +32,4 @@ Met vriendelijke groet,<br>
 <br>
 De planning<br>
 <br>
-<img src="{{url("images/logo.jpg")}}" alt="Logo" style="width:300px;height:250px;">
 
